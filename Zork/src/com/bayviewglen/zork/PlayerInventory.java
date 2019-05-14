177b48d2aa14fc8111a4a1df789a374c782e3d80
echow <echow@echowsp4.bayviewglen.ca>
2019-05-14 11:28:11 -0400

    i don't know how to do this. it'll take me a while
    Change-Id: I29163381a086e675276d2209bf9aac05e54a1d5d

---
+++ Zork/src/com/bayviewglen/zork/PlayerInventory.java
@@ -0,0 +1,18 @@
+package com.bayviewglen.zork;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerInventory {
+
+	List<String> inventory = new ArrayList<String>();
+
+	boolean backpack = false; // I don't know where to put this
+
+	if(inventory.size>20)inventory.remove();else inventory.size=inventory.size;
+
+	if(backpack=false)return null;else inventory.size=50;
+
+}
+
+}
