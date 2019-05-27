8079927e097f90a1437bb232ea7e44b3ea3febbd
aakilan <aakilan@ambihaisp4.bayviewglen.ca>
2019-05-27 10:52:31 -0400

    EAT AND TAKE METHODS FINISHED

--- Zork/src/com/bayviewglen/zork/Player.java
+++ Zork/src/com/bayviewglen/zork/Player.java
@@ -3,21 +3,40 @@ package com.bayviewglen.zork;
 public class Player {
 	PlayerInventory inventory = new PlayerInventory(0);
 	int inventoryWeight = 0;
+
 	public Player(int invenWeight) {
 		inventory = new PlayerInventory(invenWeight);
 	}
-	
-	//eat method
-	
+
+	// eat method
+
 	public String eat(food food) {
-		System.out.println("you just ate " + food + " : " + food.getSound());
-		//need if statement to check if it's in their inventory
-			//remove the food from the player inventory
-		//else: check if it's in the room, remove from room inventory
+		if(checkPlayerInventory(food) == true) {
+			removePlayerInventory(food);
+			System.out.println("You just ate " + food + " : " + food.getSound());
+		}else if(checkRoomInventory(food) == true) {
+			removeRoomInventory(food);
+			System.out.println("You just ate " + food + " : " + food.getSound());
+		}else {
+			return System.out.println("You cannot eat" + food + "because it cannot be found.")
+		}
 		
 	}
+
+	public String take(tools tool) {
+		if(checkRoomInventory(tool) = true) {
+			addPlayerInventory(tool);
+			return System.out.println(tool + "added to your Inventory.");
+		} else {return System.out.println(tool + "is not in this room. It cannot be added to your inventory.");
+	}
+
+	public String take(useless tool) {
+		if(checkRoomInventory(tool) = true) {
+				addPlayerInventory(tool);
+				return System.out.println(tool + "added to your Inventory.");
+			} else {return System.out.println(tool + "is not in this room. It cannot be added to your inventory.");
+		}
 	
-	//take
 	
 	//use
 	
