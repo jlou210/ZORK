1f64e3841e0242b48502a39716f9d62060f912f7
aakilan <aakilan@ambihaisp4.bayviewglen.ca>
2019-05-27 19:54:45 -0400

    get method is done

--- Zork/src/com/bayviewglen/zork/Player.java
+++ Zork/src/com/bayviewglen/zork/Player.java
@@ -11,16 +11,16 @@ public class Player {
 	// eat method
 
 	public String eat(food food) {
-		if(checkPlayerInventory(food) == true) {
+		if (checkPlayerInventory(food) == true) {
 			removePlayerInventory(food);
 			System.out.println("You just ate " + food + " : " + food.getSound());
-		}else if(checkRoomInventory(food) == true) {
+		} else if (checkRoomInventory(food) == true) {
 			removeRoomInventory(food);
 			System.out.println("You just ate " + food + " : " + food.getSound());
-		}else {
+		} else {
 			return "You cannot eat" + food + "because it cannot be found.";
 		}
-		
+
 	}
 
 	public String take(tools tool) {
@@ -30,34 +30,47 @@ public class Player {
 		} else {return tool + "is not in this room. It cannot be added to your inventory.";
 	}
 
-	public String take(useless tool) {
-		if(checkRoomInventory(tool) = true) {
-				addPlayerInventory(tool);
-				return tool + "added to your Inventory.";
-			} else {return tool + "is not in this room. It cannot be added to your inventory.";
+	public String take(useless item){
+		if(checkRoomInventory(item) = true) {
+				addPlayerInventory(item);
+				removeRoomInventory(item);
+				return item + "added to your Inventory.";
+			} else {return item + "is not in this room. It cannot be added to your inventory.";
 			}
 		}
 	
-	public String use(useless tool) {
-		return tool + "cannot be used here.";
+	public String use(useless item1) {
+		return item1 + "cannot be used here.";
 	}
 	
-	public String use(tools tool) {
-		if(checkPlayerInventory(tool) == true) {
-			removePlayerInventory(tool);
-			System.out.println("You just ate " + tool + " : " + food.getSound());
-		}else if(checkRoomInventory(tool) == true) {
-			removeRoomInventory(tool);
-			System.out.println("You just ate " + tool + " : " + food.getSound());
+	public String use(tools tool1) {
+		if(checkPlayerInventory(tool1) == true) {
+			System.out.println("You just used" + tool1);
+		}else if(checkRoomInventory(tool1) == true) {
+			removeRoomInventory(tool1);
+			System.out.println("You just used " + tool1);
+			addRoomInventory(tool1);
 		}else {
-			return "You cannot eat" + tool + "because it cannot be found.";
+			return "You cannot use" + tool1 + "because it cannot be found.";
 		}
 		
 	}
 	
+	public String get(tools tool2) {
+		if(checkRoomInventory = true) {
+			addPlayerInventory(tool2);
+		}else { 
+			System.out.println("You cannot get" + tool2 + "because it is not in this room.");
+		}
+		}
 	
-	
-	
-	
-	
+	public String get(useless item2)){
+		if(checkRoomInventory = true) {
+			addPlayerInventory(item2);
+		}else { 
+			System.out.println("You cannot get" + item2 + "because it is not in this room.");
+		}
+		}
+	}
+
 }
