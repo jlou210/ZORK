ce594bf9d82f6d32bc39878c249e472a4bb6cd3e
echow <echow@echowsp4.bayviewglen.ca>
2019-05-28 12:43:43 -0400

    More items

---
+++ Zork/src/com/bayviewglen/zork/Flashlight.java
@@ -0,0 +1,8 @@
+package com.bayviewglen.zork;
+
+public class Flashlight extends Tool {
+	public Flashlight(String name, int weight, String description) {
+		super(name, weight, description);
+	}
+
+}
---
+++ Zork/src/com/bayviewglen/zork/Glasses.java
@@ -0,0 +1,8 @@
+package com.bayviewglen.zork;
+
+public class Glasses extends Tool {
+	public Glasses(String name, int weight, String description) {
+		super(name, weight, description);
+	}
+
+}
