2ae951c4db7693b7eaf08d000f6435b3692825dd
echow <echow@echowsp4.bayviewglen.ca>
2019-05-28 10:14:16 -0400

    I initialized the items but tools isn't complete because I don't know what the other tools are that are used in the game.

--- Zork/src/com/bayviewglen/zork/Item.java
+++ Zork/src/com/bayviewglen/zork/Item.java
@@ -17,12 +17,25 @@ public class Item {
 	HashMap<Item, Integer> food = new HashMap<Item, Integer>();
 	HashMap<Item, Integer> tools = new HashMap<Item, Integer>();
 	
-	Item flower = new Item("flower", 5, "You picked up a blue flower. It smells pretty good, but I'm not sure if it's useful...");
+	Item flower = new Item("flower", 5, "You picked up a sunflower. The pollen is making your nose itchy. I'm not sure it's useful...");
+	Item treeBranch = new Item("tree branch", 10, "You picked up a tree branch. It's long and pointy, but I don't think it's very useful.");
+	Item sand = new Item("sand", 5, "You picked up some sand. Ahh...I wish I was at the beach right now.");
+	Item pillow = new Item("pillow", 10, "You picked up a pillow. It's so soft and fluffy like a marshmallow.");
+	Item toyTruck = new Item("toy truck", 5, "You picked up a truck. Vroom! Vroom!");
+	
+	Empanada empanada = new Empanada("empanada", 5, "You picked up an empanada. Muy deliciosa!");
+	Pie pie = new Pie("pie", 10, "You picked up a pie. Is it apple just like how grandma makes it?");
+	IceCream iceCream = new IceCream("ice cream", 5, "You picked up ice cream. Quick! Eat it before it melts!");
+	Cookies cookies = new Cookies("cookies", 5, "You picked up some cookies. Smells super good! I think they're homemade.");
+	Apple apple = new Apple("apple", 5, "You picked up an apple. Hopefully it's not poisonous.");
+	Cake cake = new Cake("cake", 20, "You picked up a 10 tier funfetti cake! It looks AMAZING!!! Eat this to win the game! Time to celebrate!");
+	
+	Map map = new Map("map", 1, "You picked up a map. This will help you navigate and teleporate back to the treehouse.");
 	
 	public void initializeItems() {
 
 		useless.put(flower, 5); // dora
-		useless.put("branch", 10); // treehouse
+		useless.put("tree branch", 10); // treehouse
 		useless.put("sand", 5); // backyard
 		useless.put("pillow", 10); // harry
 		useless.put("toy truck", 5); // max
@@ -72,8 +85,8 @@ public class Item {
 		return weight;
 	}
 	
-	public String getItemDescription() {
-		return description;
+	public String getItemDescription(Item item) {
+		return item.description;
 	}
 	
 	
---
+++ Zork/src/com/bayviewglen/zork/Map.java
@@ -0,0 +1,8 @@
+package com.bayviewglen.zork;
+
+public class Map extends Tool {
+	public Map(String name, int weight, String description) {
+		super(name, weight, description);
+	}
+
+}
