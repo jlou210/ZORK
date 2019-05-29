40c0af9f4f1ae9e94a0ed75f7e0cf5f81850e376
aakilan <aakilan@ambihaisp4.bayviewglen.ca>
2019-05-29 13:33:47 -0400

    finished descriptions for arthur and DW

--- Zork/src/com/bayviewglen/zork/DW.java
+++ Zork/src/com/bayviewglen/zork/DW.java
@@ -1,7 +1,7 @@
 package com.bayviewglen.zork;
 
 public class DW implements Character {
-	String description = "description";
+	String description = "Arthur can't seem to find his glasses, do you think you can find them for him?";
 	public void speak(){
 		System.out.println(description);
 	}
