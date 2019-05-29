0950f0a59f3550c808bbcd06f3e0f77ef0322097
echow <echow@echowsp4.bayviewglen.ca>
2019-05-29 14:44:28 -0400

    I'm getting anxiety

--- Zork/src/com/bayviewglen/zork/LeafletHint.java
+++ Zork/src/com/bayviewglen/zork/LeafletHint.java
@@ -4,5 +4,9 @@ public class LeafletHint extends Tool {
 	public LeafletHint(String name, int weight, String description) {
 		super(name, weight, description);
 	}
+	
+	public String message() {
+		return "Search for a hidden gem where you life is created.";
+	}
 
 }
