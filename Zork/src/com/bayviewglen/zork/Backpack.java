07790f96668178edfb410c6eee72a0c01dfd56dc
echow <echow@echowsp4.bayviewglen.ca>
2019-05-29 14:25:41 -0400

    Backpack class

---
+++ Zork/src/com/bayviewglen/zork/Backpack.java
@@ -0,0 +1,8 @@
+package com.bayviewglen.zork;
+
+public class Backpack extends Tool {
+	public Backpack(String name, int weight, String description) {
+		super(name, weight, description);
+	}
+
+}
--- Zork/src/com/bayviewglen/zork/Item.java
+++ Zork/src/com/bayviewglen/zork/Item.java
@@ -24,6 +24,7 @@ public class Item {
 	static Item sand = new Item("sand", 5, "You picked up some sand. Ahh...I wish I was at the beach right now.");
 	static Item pillow = new Item("pillow", 10, "You picked up a pillow. It's so soft and fluffy like a marshmallow.");
 	static Item toyTruck = new Item("toy truck", 5, "You picked up a truck. Vroom! Vroom!");
+	static Item clothes = new Item("clothes", 10, "You picked up some clothes, but the outfit you are wearing is fine."); 
 	
 	static Empanada empanada = new Empanada("empanada", 5, "You picked up an empanada. Muy deliciosa!");
 	static Pie pie = new Pie("pie", 10, "You picked up a pie. Is it apple just like how grandma makes it?");
@@ -38,7 +39,8 @@ public class Item {
 	static Book book = new Book("book", 5, "You picked up a book. You can read it."); 
 	static LeafletHint leafletHint = new LeafletHint("leaflet hint", 1, "You picked up a leaflet hint. Hmm..."); 
 	static LibraryCard libraryCard = new LibraryCard("library card", 1, "You picked up a library card. Having fun isn't hard."); 
-	static Clothes clothes = new Clothes("clothes", 10, "You picked up some clothes, but the outfit you are wearing is fine."); 
+	static Backpack backpack = new Backpack("backpack", 1, "You picked up a backpack. You can use it hold all your items!"); 
+	
 	
 	public static void initializeItems() {
 
@@ -64,6 +66,7 @@ public class Item {
 		tools.put("book", book); // arthur
 		tools.put("map", map); //dora 
 		tools.put("leaflet hint", leafletHint);
+		tools.put("backpack", backpack); 
 		
 	}
 	
