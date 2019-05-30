73315841cb76c4b6eb8144c0e183520b7f61a903
jlou <jlou@JLOUSPRO.bayviewglen.ca>
2019-05-30 15:21:48 -0400

    inventory fix

--- Zork/src/com/bayviewglen/zork/Inventory.java
+++ Zork/src/com/bayviewglen/zork/Inventory.java
@@ -76,31 +76,7 @@ public class Inventory {
 
 	}
 
-	public void initializeRooms() {
-		//dora
-		rooms[0] = new String [] {"House Entrance","map", "Dora"};
-		rooms[1] = new String [] {"Dining Room", "pie", "ice cream"};
-		rooms[2] = new String [] {"Kitchen"};
-		rooms[3] = new String [] {"LivingRoom", "backpack", "clothes", "leaflet hint"};
-		//arthur
-		rooms[4] = new String [] {"Arthur's Room", "glasses"};
-		rooms[5] = new String [] {"Mr.Ratburn's Class", "ice cream"};
-		rooms[6] = new String [] {"Study Room", "apple"};
-		rooms[7] = new String [] {"Library", "bookshelf key"};
-		rooms[8] = new String [] {"Secret Room", "flashlight", "trophy"};
-		//backyardigans
-		rooms[9] = new String [] {"Mini Garden", "dirt", "library card", "tree branch", "flower"};
-		rooms[10] = new String [] {"Backyard", "sand"};
-		rooms[11] = new String [] {"Back Storage Room", "shed key"};
-		//max and ruby
-		rooms[12] = new String [] {"Max's Room", "ant farm", "empanada", "toy truck"};
-		rooms[13] = new String [] {"Ruby's Room", "pillow"};
-		//treehouse
-		rooms[14] = new String [] {"Pantry", "cookies", "donuts"};
-		//mickeymouse
-		rooms[15] = new String [] {"Win Room", "cake"};		
 
-	}
 
 	public void initializeRooms() {
 		// dora
