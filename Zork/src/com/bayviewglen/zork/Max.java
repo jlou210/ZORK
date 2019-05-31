72321a6bdf7aadf659524f15a8eb393bfd3ff655
echow <echow@echowsp4.bayviewglen.ca>
2019-05-31 16:33:23 -0400

    Max giving trophy

--- Zork/src/com/bayviewglen/zork/Max.java
+++ Zork/src/com/bayviewglen/zork/Max.java
@@ -1,8 +1,19 @@
 package com.bayviewglen.zork;
 
 public class Max implements Character{
-	String description = "Hi!";
-	public void speak(){
-		System.out.println(description);
+	String description = "You found it! Here's an amazing and shiny trophy I made for you!";
+	public void speakOne(){
+		System.out.println("Hi. Hi. I can't seem to find my ant farm. Can you find it? I'll give you a prize in return.");	//speech pattern when glasses are not given
+	}
+	
+	public void speak() {
+		System.out.println(description);	//after he gives glasses
+	}
+	
+	public boolean isGlasses(Inventory playerInven ) {
+		if(playerInven.checkPlayerInventory(Item.tools.get("ant farm"))){
+			return true;
+		}
+		return false;
 	}
 }
