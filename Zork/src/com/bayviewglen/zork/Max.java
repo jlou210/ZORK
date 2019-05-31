696973bcbed09e5cfdea0688db1aea463a32df2a
echow <echow@echowsp4.bayviewglen.ca>
2019-05-31 17:21:32 -0400

    I'm trying to remove trophy from room and add it to player inventory

--- Zork/src/com/bayviewglen/zork/Max.java
+++ Zork/src/com/bayviewglen/zork/Max.java
@@ -1,13 +1,18 @@
 package com.bayviewglen.zork;
 
+import java.util.HashMap;
+
 public class Max implements Character{
 	String description = "You found it! Here's an amazing and shiny trophy I made for you!";
 	public void speakOne(){
 		System.out.println("Hi. Hi. I can't seem to find my ant farm. Can you find it? I'll give you a prize in return.");	//speech pattern when glasses are not given
 	}
 	
-	public void speak() {
-		System.out.println(description);	//after he gives glasses
+	public void speak(Game game, Item trophy2) {
+		System.out.println(description); //after he gives trophy
+		HashMap<String, Room> hashmap = game.getMasterMap();
+		hashmap.remove(trophy2);	
+		
 	}
 	
 	public boolean isGlasses(Inventory playerInven ) {
