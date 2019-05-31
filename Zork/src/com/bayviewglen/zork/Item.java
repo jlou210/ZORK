9f8b202bf0a0de12a78210aefce5cdbb23559872
echow <echow@echowsp4.bayviewglen.ca>
2019-05-31 12:58:53 -0400

    more

--- Zork/src/com/bayviewglen/zork/Item.java
+++ Zork/src/com/bayviewglen/zork/Item.java
@@ -6,7 +6,7 @@ public class Item {
 	
 	public String name;
 	public int weight;
-	public static String description;
+	public String description;
 	
 	public Item(String name, int weight, String description) {
 		this.name = name;
